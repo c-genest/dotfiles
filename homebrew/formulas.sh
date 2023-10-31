190431c2fb214f77ad15e87acad614fd869c9468
Chris GENEST <chris.genest@gmail.com>
2023-10-31 09:39:34 +0100

    Clean formulae

--- homebrew/formulas.sh
+++ homebrew/formulas.sh
@@ -7,7 +7,6 @@ brew upgrade
 brew tap homebrew/cask
 brew tap homebrew/cask-versions
 
-brew install --cask adoptopenjdk11
 brew install --cask google-cloud-sdk
 brew install --cask dropbox
 brew install --cask firefox
@@ -37,6 +36,8 @@ brew install pnpm
 brew install ansible
 brew install bash-completion
 brew install jq
+brew install yq
+brew install keka
 brew install kubernetes-cli
 brew install httpie
 brew install watch
@@ -47,7 +48,6 @@ brew install pv
 brew install bat
 brew install diff-so-fancy
 brew install tldr
-brew install fd
 brew install coreutils
 brew install telnet
 brew install stern
